397cff04aad52d708f6a2997a06bc14a365a6a5f
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-09-16 20:06:20 -0600

    started dipping into using state syntax

--- src/components/TargetArea.js
+++ src/components/TargetArea.js
@@ -3,10 +3,13 @@ import { View, Text } from 'react-native'
 import axios from 'axios'
 
 class TargetArea extends Component {
+  state = { TargetArea: [] }
+
+
 
   componentWillMount() {
     axios.get('https://galvaniizereads.herokuapp.com/books')
-      .then(response => console.log(response))
+      .then(response => this.setState({ TargetArea: response.data }))
   }
 
   render () {

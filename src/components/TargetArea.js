aa61f7e75bc1c6d6e5d79337677bbe5d92d3a614
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-09-18 12:21:41 -0600

    working API

--- src/components/TargetArea.js
+++ src/components/TargetArea.js
@@ -3,31 +3,21 @@ import { View, Text } from 'react-native'
 import axios from 'axios'
 
 class TargetArea extends Component {
-  state = { TargetArea: [] }
-
-
-
   componentWillMount() {
-    axios.get('https://galvaniizereads.herokuapp.com/books')
-      .then(response => this.setState({ TargetArea: response.data }))
-  }
-
-  renderTargetArea() {
-    return this.state.TargetArea.map(TargetAreas => <Text>{TargetAreas.name}</Text>)
+    axios.get('https://shapesserver.herokuapp.com/targetarea')
+      .then(response => console.log(response))
   }
 
 
 
-
-  render () {
-    console.log(this.state)
-
+  render() {
     return (
       <View>
-        <Text>Target Area!!!!!!!!</Text>
+        <Text>TARGET AREA</Text>
       </View>
     )
   }
 }
+
 
 export default TargetArea
